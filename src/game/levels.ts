8635291b91da61f29d3f8bc9709ecfdb0b9130fa
Frenk0371 <scelsa.f@gmail.com>
2026-09-03 07:35:15 +0200

    Aggiunge il quinto livello

--- src/game/levels.ts
+++ src/game/levels.ts
@@ -3,10 +3,10 @@ export type LevelConfig = {
   moves: number
   targetScore: number
   objective: 'score' | 'collect' | 'collectDouble'
-collectType?: number
-collectAmount?: number
-collectType2?: number
-collectAmount2?: number
+  collectType?: number
+  collectAmount?: number
+  collectType2?: number
+  collectAmount2?: number
 }
 
 export const levels: LevelConfig[] = [
@@ -17,28 +17,37 @@ export const levels: LevelConfig[] = [
     objective: 'score'
   },
   {
-  level: 2,
-  moves: 20,
-  targetScore: 6000,
-  objective: 'score'
-},
-{
-  level: 3,
-  moves: 19,
-  targetScore: 7000,
-  objective: 'collect',
-  collectType: 2,
-  collectAmount: 9,
-},
-{
-  level: 4,
-  moves: 20,
-  targetScore: 0,
-  objective: 'collectDouble',
-  collectType: 0,
-  collectAmount: 9,
-  collectType2: 3,
-  collectAmount2: 9,
-},
+    level: 2,
+    moves: 20,
+    targetScore: 6000,
+    objective: 'score'
+  },
+  {
+    level: 3,
+    moves: 19,
+    targetScore: 7000,
+    objective: 'collect',
+    collectType: 2,
+    collectAmount: 9,
+  },
+  {
+    level: 4,
+    moves: 20,
+    targetScore: 0,
+    objective: 'collectDouble',
+    collectType: 0,
+    collectAmount: 9,
+    collectType2: 3,
+    collectAmount2: 9,
+  },
+  {
+    level: 5,
+    moves: 18,
+    targetScore: 0,
+    objective: 'collectDouble',
+    collectType: 1,
+    collectAmount: 12,
+    collectType2: 4,
+    collectAmount2: 12,
+  },
 ]
-
