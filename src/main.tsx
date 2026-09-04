44790bf4f019d4f8b27aa85345546c893c0e7056
Frenk0371 <scelsa.f@gmail.com>
2026-09-04 08:02:25 +0200

    Attiva gameplay avanzato dei nuovi mondi

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "@fontsource/fredoka/700.css";
 import "@fontsource/lilita-one/400.css";
 import "./index.css";
 import "./visualTweaks";
+import "./worldGameplayTweaks";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
