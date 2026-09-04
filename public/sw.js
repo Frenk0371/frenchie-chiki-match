e54974a7cd768366cdfe7cf67b2396ac42fe16f7
Frenk0371 <scelsa.f@gmail.com>
2026-09-04 10:26:49 +0200

    Aggiorna cache per avatar Chiki

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'frenchie-chiki-match-v21'
+const CACHE_NAME = 'frenchie-chiki-match-v22'
 const APP_SHELL = ['/', '/chiki-icon.jpeg', '/manifest.webmanifest', '/level-lock-v2.png', '/menu-adventure.png', '/menu-trophy.png']
 
 self.addEventListener('install', event => {
