21d4c42c2eb59d4b78482d80c0b157ebd0613697
Frenk0371 <scelsa.f@gmail.com>
2026-09-04 19:31:19 +0200

    Aggiorna cache per regali e banana vita

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'frenchie-chiki-match-v500-r19'
-const APP_SHELL = ['/', '/chiki-icon.jpeg', '/manifest.webmanifest', '/level-lock-v2.png', '/menu-adventure.png', '/menu-trophy.png', '/special-grid-rocket.svg', '/special-bomb.svg', '/special-rainbow.svg']
+const CACHE_NAME = 'frenchie-chiki-match-v500-r20'
+const APP_SHELL = ['/', '/chiki-icon.jpeg', '/manifest.webmanifest', '/level-lock-v2.png', '/menu-adventure.png', '/menu-trophy.png', '/special-grid-rocket.svg', '/special-bomb.svg', '/special-rainbow.svg', '/banana-life.svg']
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)))
