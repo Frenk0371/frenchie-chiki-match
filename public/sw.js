1a75daa0655ab77eaef9da31f6b4a8457ebe6375
Frenk0371 <scelsa.f@gmail.com>
2026-09-04 07:24:19 +0200

    Aggiorna cache PWA per il nuovo logo

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'frenchie-chiki-match-v6'
+const CACHE_NAME = 'frenchie-chiki-match-v7'
 const APP_SHELL = ['/', '/chiki-icon.jpeg', '/manifest.webmanifest', '/level-lock-v2.png', '/menu-adventure.png', '/menu-trophy.png']
 
 self.addEventListener('install', event => {
