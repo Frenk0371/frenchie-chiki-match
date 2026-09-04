9f7c21b63bca9ad002044de0ebc78af0060950cd
Frenk0371 <scelsa.f@gmail.com>
2026-09-04 23:51:47 +0200

    Rafforza background audio Music House iOS

--- MusicHouse-iOS/MusicHouse/BackgroundAudioSession.swift
+++ MusicHouse-iOS/MusicHouse/BackgroundAudioSession.swift
@@ -1,18 +1,54 @@
 import AVFoundation
 import Foundation
+import UIKit
 
 final class BackgroundAudioSession {
     static let shared = BackgroundAudioSession()
 
-    private init() {}
+    private let session = AVAudioSession.sharedInstance()
+
+    private init() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleInterruption),
+            name: AVAudioSession.interruptionNotification,
+            object: session
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleRouteChange),
+            name: AVAudioSession.routeChangeNotification,
+            object: session
+        )
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
 
     func activate() {
-        let session = AVAudioSession.sharedInstance()
         do {
             try session.setCategory(.playback, mode: .default, options: [.allowAirPlay])
             try session.setActive(true)
+            UIApplication.shared.beginReceivingRemoteControlEvents()
         } catch {
             print("Music House audio session error: \(error)")
         }
+    }
+
+    @objc private func handleInterruption(_ notification: Notification) {
+        guard
+            let info = notification.userInfo,
+            let rawType = info[AVAudioSessionInterruptionTypeKey] as? UInt,
+            let type = AVAudioSession.InterruptionType(rawValue: rawType)
+        else { return }
+
+        if type == .ended {
+            activate()
+        }
+    }
+
+    @objc private func handleRouteChange(_ notification: Notification) {
+        activate()
     }
 }
