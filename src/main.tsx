c0086f837478a1e5b2a24df50a9dfc7d709efb92
Frenk0371 <scelsa.f@gmail.com>
2026-09-04 08:20:33 +0200

    Carica le rifiniture della release da 100 livelli

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import "@fontsource/fredoka/600.css";
 import "@fontsource/fredoka/700.css";
 import "@fontsource/lilita-one/400.css";
 import "./index.css";
+import "./release100.css";
 import "./visualTweaks";
 import "./worldGameplayTweaks";
 import "./matchEffectsTweaks";
+import "./release100Tweaks";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
