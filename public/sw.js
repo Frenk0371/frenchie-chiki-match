5a21ba15201f3503ec826f50848905169cd5d2f6
Frenk0371 <scelsa.f@gmail.com>
2026-09-04 19:15:52 +0200

    Aggiorna cache PWA per animazioni aiuti integrate

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'frenchie-chiki-match-v500-r16'
+const CACHE_NAME = 'frenchie-chiki-match-v500-r17'
 const APP_SHELL = ['/', '/chiki-icon.jpeg', '/manifest.webmanifest', '/level-lock-v2.png', '/menu-adventure.png', '/menu-trophy.png', '/special-grid-rocket.svg', '/special-bomb.svg', '/special-rainbow.svg']
 
 self.addEventListener('install', event => {
