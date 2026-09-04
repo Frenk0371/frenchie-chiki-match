1b494efb0886fed2d2363be4279bef48b876d2da
Frenk0371 <scelsa.f@gmail.com>
2026-09-04 12:38:19 +0200

    Forza cache bust per release 500

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'frenchie-chiki-match-v27'
+const CACHE_NAME = 'frenchie-chiki-match-v500-r2'
 const APP_SHELL = ['/', '/chiki-icon.jpeg', '/manifest.webmanifest', '/level-lock-v2.png', '/menu-adventure.png', '/menu-trophy.png']
 
 self.addEventListener('install', event => {
@@ -7,15 +7,26 @@ self.addEventListener('install', event => {
 })
 
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))))
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+  )
   self.clients.claim()
 })
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return
-  event.respondWith(fetch(event.request).then(response => {
-    const copy = response.clone()
-    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy))
-    return response
-  }).catch(() => caches.match(event.request).then(response => response || caches.match('/'))))
+
+  const networkRequest = event.request.mode === 'navigate'
+    ? new Request(event.request, { cache: 'reload' })
+    : event.request
+
+  event.respondWith(
+    fetch(networkRequest)
+      .then(response => {
+        const copy = response.clone()
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy))
+        return response
+      })
+      .catch(() => caches.match(event.request).then(response => response || caches.match('/')))
+  )
 })
