f539b3589271c46969fc704b10c7e4684afde971
Frenk0371 <scelsa.f@gmail.com>
2026-09-04 08:56:58 +0200

    Carica stile saldo Monete Chiki

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "@fontsource/fredoka/700.css";
 import "@fontsource/lilita-one/400.css";
 import "./index.css";
 import "./release100.css";
+import "./HomeEconomy.css";
 import "./visualTweaks";
 import "./worldGameplayTweaks";
 import "./matchEffectsTweaks";
